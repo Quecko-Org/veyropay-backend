ce6e258158ae27f0453697a2953d3af7a1b7d7cc
Mujahid Khan <immujahidkhan6@gmail.com>
2026-09-14 11:57:57 +0500

    👍

--- src/modules/recovery/recovery.service.spec.ts
+++ src/modules/recovery/recovery.service.spec.ts
@@ -26,7 +26,7 @@ describe('RecoveryService', () => {
   const guardianUser = {
     id: guardianUserId,
     email: 'mark1@example.com',
-    displayName: 'Mark de Vries',
+    displayName: 'Mark Vries',
     status: UserStatus.ACTIVE,
   };
 
